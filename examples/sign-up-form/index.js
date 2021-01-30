8e33e1ea9a3621e53a63220e809400d74e3d66f4
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-01-30 15:14:09 +0100

    Example: Sign up form

---
+++ examples/sign-up-form/index.js
@@ -0,0 +1,34 @@
+import { createTemplate } from '../../createTemplate.js'
+
+const template = createTemplate(`
+  <template>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </template>
+`)
+
+class SignUpForm extends HTMLElement {
+  constructor() {
+    super()
+    const templateContent = template.content
+    const shadowRoot = this.attachShadow({mode: 'closed'})
+    shadowRoot.appendChild(templateContent.cloneNode(true))
+  }
+}
+
+customElements.define('sign-up-from', SignUpForm)
